ed10ec441ae275ce2e82c0a518855d691aaa733d
Shubh Maheshwari <shubhmaheshwari8193@email.com>
2024-07-01 17:13:34 +0530

    Project Completed.

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,3 +12,8 @@ const kBottomContainerColour = Color(0xFFEB1555);
 
 const kLabelTextStyle = TextStyle(color: Color(0xFF8D8E98), fontSize: 18.0);
 const kNumberTextStyle = TextStyle(fontSize: 50.0,fontWeight: FontWeight.w900);
+const kLargeButtonTextStyle = TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold);
+const kTitleTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold);
+const kResultTextStyle = TextStyle(fontSize: 28, color: Colors.green);
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+const kBodyTextStyle = TextStyle(fontSize: 22.0);
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,68 @@
+import 'package:bmi/constants.dart';
+import 'package:bmi/screens/input_page.dart';
+import 'package:bmi/components/reusable_card.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+
+  ResultsPage({required this.bmiResult, required this.resultText, required this.interpretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: Container(
+            child: Center(
+                child: Text(
+              'Your Result',
+              style: kTitleTextStyle,
+            )),
+          )),
+          Expanded(
+            flex: 5,
+            child: ReusabeCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(resultText.toLowerCase() , style: kResultTextStyle,),
+                  Text(bmiResult, style: kBMITextStyle,),
+                  Text(interpretation,style: kBodyTextStyle, textAlign: TextAlign.center,)
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => InputPage()));
+              Navigator.pop(context);
+            },
+            child: Container(
+              child: Center(
+                  child: Text(
+                'RE-CALCULATE',
+                style: kLargeButtonTextStyle,
+              )),
+              color: kBottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              // padding: EdgeInsets.only(bottom: 20.0),
+              height: kBottomContainerHeight,
+              width: double.infinity,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
